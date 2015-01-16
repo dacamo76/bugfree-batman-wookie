9da54bb6b48a169ef118c0b593174244a3bb99b9
Daniel Cañas <daniel_76@yahoo.com>
2015-01-16 13:50:25 -0600

    Add redis backend

--- src/bugfree_batman_wookie/core/handler.clj
+++ src/bugfree_batman_wookie/core/handler.clj
@@ -1,15 +1,21 @@
 (ns bugfree-batman-wookie.core.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [taoensso.carmine :as car]))
 
 (def counter (atom 0))
+(def conn
+  (let [uri (System/getenv "REDISTOGO_URL")]
+    {:spec {:uri uri}}))
 
 (defn increment [] (swap! counter inc))
+(defn redis-increment [] (car/wcar conn (car/incr "hits")))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
-  (GET "/count" [] (str (increment)))
+  (GET "/count" [] (do (println conn) (str (redis-increment))))
+;  (GET "/count" [] (str (increment)))
   (route/not-found "Not Found"))
 
 (def app
